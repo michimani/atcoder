c3e6ab9dba3c5ede57f4777049cc86a1f910499f
michimani <michimani210@gmail.com>
2023-10-02 23:33:31 +0900

    abs: ABC081A

---
+++ abs/ABC081A/cpp/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  string s;
+  cin >> s;
+
+  int count = 0;
+  for (char c : s)
+  {
+    if (c == '1')
+    {
+      count++;
+    }
+  }
+
+  cout << count << endl;
+}
