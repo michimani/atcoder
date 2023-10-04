cfd0467058b3003dd3c31cecc6157f203364998c
michimani <michimani210@gmail.com>
2023-10-04 09:05:24 +0900

    abs: ABC085C

---
+++ abs/ABC085C/cpp/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int N, Y = 0;
+  cin >> N >> Y;
+
+  for (int x = 0; x <= N; x++)
+  {
+    for (int y = 0; y <= N - x; y++)
+    {
+      int z = N - x - y;
+      if (10000 * x + 5000 * y + 1000 * z == Y)
+      {
+        cout << x << " " << y << " " << z << endl;
+        return 0;
+      }
+    }
+  }
+
+  cout << "-1 -1 -1" << endl;
+}
