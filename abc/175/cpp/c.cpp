b1dbd369519f19a715bb0c490b3b2465f2fdc993
michimani <michimani210@gmail.com>
2023-10-05 23:21:27 +0900

    abc: 175 C

---
+++ abc/175/cpp/c.cpp
@@ -0,0 +1,22 @@
+// https://atcoder.jp/contests/abc175/tasks/abc175_c
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  long long X = 0, K = 0, D = 0;
+  cin >> X >> K >> D;
+
+  long long k = abs(X) / D;
+  if (K < k)
+  {
+    k = K;
+  }
+  else if ((K - k) % 2 == 1)
+  {
+    k++;
+  }
+
+  cout << abs(abs(X) - D * k) << endl;
+}
