74cd9d82682af4c17f15a408a4df180f3e207c56
michimani <michimani210@gmail.com>
2023-10-11 00:08:21 +0900

    abc: 208 c

---
+++ abc/208/README.md
@@ -0,0 +1,4 @@
+AtCoder Beginner Contest 208
+===
+
+[問題 - AtCoder Beginner Contest 208](https://atcoder.jp/contests/abc208/tasks)
---
+++ abc/208/cpp/c.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+  long long N = 0, K = 0;
+  cin >> N >> K;
+
+  vector<long long> nums;
+  map<long long, long long> nums_idx_map;
+  map<long long, long long> nums_count_map;
+  for (long long i = 0; i < N; i++)
+  {
+    long long a = 0;
+    cin >> a;
+    nums.push_back(a);
+    nums_idx_map[i] = a;
+    nums_count_map[a] = 0;
+  }
+
+  sort(nums.begin(), nums.end());
+
+  long long base = K / N;
+  long long remain = K - (N * base);
+  for (long long i = 0; i < remain; i++)
+  {
+    nums_count_map[nums[i]]++;
+  }
+
+  for (long long i = 0; i < N; i++)
+  {
+    cout << nums_count_map[nums_idx_map[i]] + base << endl;
+  }
+}
