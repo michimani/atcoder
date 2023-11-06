afd50fc2e912a7d80604d72cbe6fcbea557d41e7
michimani <michimani210@gmail.com>
2023-11-06 14:33:08 +0900

    abc: 160 c

--- abc/160/cpp/c.cpp
+++ abc/160/cpp/c.cpp
@@ -6,22 +6,18 @@ using namespace std;
 
 int main()
 {
-  int k, n;
+  int k = 0;
+  unsigned int n = 0;
   cin >> k >> n;
 
   vector<int> A(n, 0);
-  int max_a = 0;
-  int min_a = INT16_MAX;
-  for (int i = 0; i < n; i++)
+  for (unsigned int i = 0; i < n; i++)
   {
     cin >> A[i];
-
-    max_a = max(max_a, A[i]);
-    min_a = min(min_a, A[i]);
   }
 
   sort(A.begin(), A.end());
-  int ans = max_a - min_a;
+  int ans = A[A.size() - 1] - A[0];
   for (unsigned int i = 1; i < A.size(); i++)
   {
     ans = min(ans, A[i - 1] + k - A[i]);
