ced1bfb78603f28756ced9bd398c380fd29104e9
michimani <michimani210@gmail.com>
2023-12-21 20:19:16 +0900

    abc: 318 c

---
+++ abc/301-400/318/README.md
@@ -0,0 +1,4 @@
+AtCoder Beginner Contest 318
+===
+
+[問題 - AtCoder Beginner Contest 318](https://atcoder.jp/contests/abc318/tasks)
---
+++ abc/301-400/318/cpp/c.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+  unsigned int n, d;
+  unsigned long long p;
+  cin >> n >> d >> p;
+
+  vector<unsigned long long> f(n, 0);
+  unsigned long long sum = 0;
+  for (unsigned int i = 0; i < n; i++)
+  {
+    cin >> f[i];
+    sum += f[i];
+  }
+
+  sort(f.rbegin(), f.rend());
+
+  for (unsigned int i = 0; i < n; i += d)
+  {
+    unsigned long long part_sum = 0;
+    for (unsigned int ii = i; ii < min(n, i + d); ii++)
+    {
+      part_sum += f[ii];
+    }
+
+    if (part_sum > p)
+    {
+      sum = sum - part_sum + p;
+    }
+    else
+    {
+      break;
+    }
+  }
+
+  cout << sum << endl;
+}
