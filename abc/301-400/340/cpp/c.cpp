9f7f3d75adba8183aa7c7413789046e297068122
michimani <michimani210@gmail.com>
2024-02-11 00:44:27 +0900

    abc: 340 c

--- abc/301-400/340/cpp/c.cpp
+++ abc/301-400/340/cpp/c.cpp
@@ -7,25 +7,17 @@ using ull = unsigned long long;
 ull calc(ull x, map<ull, ull> &m)
 {
   if (x <= 1)
-  {
     return 0;
-  }
 
   ull s = x;
   if (m.count(x) > 0)
-  {
     return s + m[x];
-  }
 
   ull res = 0;
   if (x % 2 == 0)
-  {
     res = 2 * calc(x / 2, m);
-  }
   else
-  {
     res = calc(x / 2, m) + calc(x / 2 + 1, m);
-  }
 
   m[x] = res;
 
