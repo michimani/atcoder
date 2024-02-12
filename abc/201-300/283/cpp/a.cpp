2afb17957c578df1c55fc1ba21bc24ad9c54af51
michimani <michimani210@gmail.com>
2024-02-12 18:18:11 +0900

    abc: 283 a

---
+++ abc/201-300/283/cpp/a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+using ul = unsigned long;
+
+int main()
+{
+  ul a, b;
+  cin >> a >> b;
+
+  cout << ul(pow(a, b)) << endl;
+  return 0;
+}
