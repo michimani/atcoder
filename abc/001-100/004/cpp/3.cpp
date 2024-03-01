00f60c9f4e8d89b4ef23c01e4a1f8c135a249566
michimani <michimani210@gmail.com>
2024-03-01 23:41:43 +0900

    abc: 004 3

--- abc/001-100/004/cpp/3.cpp
+++ abc/001-100/004/cpp/3.cpp
@@ -9,14 +9,10 @@ int main()
   ui n;
   cin >> n;
 
-  ui init = n / 5;
   vector<ui> cards(6, 0);
-  cards[0] = init % 6 + 1;
+  cards[0] = (n / 5) % 6 + 1; // init
   for (ui i = 1; i < 6; i++)
-  {
-    ui c = (cards[i - 1] + 1) % 7;
-    cards[i] = (c > 0) ? c : 1;
-  }
+    cards[i] = cards[i - 1] % 6 + 1;
 
   for (ui i = 0; i < n % 5; i++)
     swap(cards[i % 5], cards[i % 5 + 1]);
