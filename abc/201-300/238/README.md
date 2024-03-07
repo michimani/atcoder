66f556e35d42b18d6bccd0cf0003cee431de0fce
michimani <michimani210@gmail.com>
2024-03-07 22:48:24 +0900

    abc: 238 c

---
+++ abc/201-300/238/README.md
@@ -0,0 +1,4 @@
+AtCoder Beginner Contest 238
+===
+
+[問題 - AtCoder Beginner Contest 238](https://atcoder.jp/contests/abc238/tasks)
---
+++ abc/201-300/238/cpp/c.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+using ui = unsigned int;
+using ull = unsigned long long;
+
+int main()
+{
+  ull n;
+  cin >> n;
+
+  ull ans = 0;
+  ull d = 10;
+  const ull div = 998244353;
+  for (ui i = 0; i < to_string(n).length(); i++)
+  {
+    ull r = min(d - 1, n);
+    if (i == 0)
+    {
+      ans += (((1 + r) * r) / 2) % div;
+    }
+    else
+    {
+      r = (r - (d / 10) + 1) % div;
+      ans += ((r * (r + 1)) / 2) % div;
+    }
+
+    d *= 10;
+  }
+
+  cout << ans % div << endl;
+  return 0;
+}
