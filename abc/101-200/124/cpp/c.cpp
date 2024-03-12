0bf9a76e5ff927cde6cd118ae99c512f0414043a
michimani <michimani210@gmail.com>
2024-03-13 00:58:50 +0900

    abc: 124 c

--- abc/101-200/124/cpp/c.cpp
+++ abc/101-200/124/cpp/c.cpp
@@ -9,7 +9,7 @@ char rev(char a)
   return (a == '0' ? '1' : '0');
 }
 
-void trav(ui &n, string &s, char prev, char curr, char next, ui pos, ui cnt, ui &ans)
+void trav(ui &n, string &s, char prev, char curr, ui pos, ui cnt, ui &ans)
 {
   if (pos == n - 1)
   {
@@ -17,21 +17,23 @@ void trav(ui &n, string &s, char prev, char curr, char next, ui pos, ui cnt, ui 
     return;
   }
 
+  char next = s[pos + 1];
+
   if (curr == next)
   {
     next = rev(next);
-    trav(n, s, curr, next, s[pos + 2], pos + 1, cnt + 1, ans);
+    trav(n, s, curr, next, pos + 1, cnt + 1, ans);
 
     next = rev(next);
     if (pos > 0 && prev == curr)
     {
       curr = rev(curr);
-      trav(n, s, curr, next, s[pos + 2], pos + 1, cnt + 1, ans);
+      trav(n, s, curr, next, pos + 1, cnt + 1, ans);
     }
   }
   else
   {
-    trav(n, s, curr, next, s[pos + 2], pos + 1, cnt, ans);
+    trav(n, s, curr, next, pos + 1, cnt, ans);
   }
 
   return;
@@ -45,7 +47,7 @@ int main()
   ui n = ui(s.length());
   ui ans = n;
 
-  trav(n, s, '.', s[0], s[1], 0, 0, ans);
+  trav(n, s, '.', s[0], 0, 0, ans);
 
   cout << ans << endl;
   return 0;
