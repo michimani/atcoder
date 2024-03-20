2a287926dd6a660f94550a680608f2ea6a5a5c54
michimani <michimani210@gmail.com>
2024-03-20 13:53:53 +0900

    abc: 345 b

--- abc/301-400/345/cpp/b.cpp
+++ abc/301-400/345/cpp/b.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
-using ui = unsigned int;
 using ll = long long;
 
 int main()
@@ -10,46 +8,22 @@ int main()
   ll x;
   cin >> x;
 
-  if (abs(x) < 10)
-  {
-    cout << (x > 0 ? 1 : 0) << endl;
-    return 0;
-  }
-
   string sx = to_string(x);
   ll d = sx[sx.length() - 1] - '0';
 
-  sx.pop_back();
-  if (x < 0 || d == 0)
-  {
-    cout << sx << endl;
-    return 0;
-  }
+  ll xx = x / 10;
 
-  reverse(sx.begin(), sx.end());
-  int add = 1;
-  for (ui i = 0; i < sx.length(); i++)
+  if (x >= 0)
   {
-    int dg = sx[i] - '0';
-    dg += add;
-    if (dg > 9)
-    {
-      add = 1;
-      dg = 0;
-    }
+    if (d == 0)
+      cout << xx << endl;
     else
-    {
-      add = 0;
-    }
-
-    sx[i] = char(dg + '0');
+      cout << xx + 1 << endl;
+  }
+  else
+  {
+    cout << xx << endl;
   }
 
-  if (add == 1)
-    sx.push_back('1');
-
-  reverse(sx.begin(), sx.end());
-
-  cout << sx << endl;
   return 0;
 }
