3530b624ca0613c4a725ab543ec76b1ce36c9d00
michimani <michimani210@gmail.com>
2024-04-29 12:55:53 +0900

    abc: 219 a

---
+++ abc/201-300/219/cpp/a.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+using ui = unsigned int;
+
+int main()
+{
+  ui x;
+  cin >> x;
+
+  if (x < 40)
+    cout << 40 - x << endl;
+  else if (x < 70)
+    cout << 70 - x << endl;
+  else if (x < 90)
+    cout << 90 - x << endl;
+  else
+    cout << "expert" << endl;
+
+  return 0;
+}
