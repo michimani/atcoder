ee0dffda3e846acf36c650d4e224322b48536303
michimani <michimani210@gmail.com>
2024-05-12 22:56:42 +0900

    abc: 008 1

---
+++ abc/001-100/008/README.md
@@ -0,0 +1,4 @@
+AtCoder Beginner Contest 008
+===
+
+[問題 - AtCoder Beginner Contest 008](https://atcoder.jp/contests/abc008/tasks)
---
+++ abc/001-100/008/cpp/1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+using ui = unsigned int;
+
+int main()
+{
+  ui s, t;
+  cin >> s >> t;
+  cout << t - s + 1 << endl;
+
+  return 0;
+}
