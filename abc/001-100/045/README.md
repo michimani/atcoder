51b5d024f345bade73a6a62bde91aa943defc5a4
michimani <michimani210@gmail.com>
2024-05-18 05:09:13 +0900

    abc: 045 a

---
+++ abc/001-100/045/README.md
@@ -0,0 +1,4 @@
+AtCoder Beginner Contest 045
+===
+
+[問題 - AtCoder Beginner Contest 045](https://atcoder.jp/contests/abc045/tasks)
---
+++ abc/001-100/045/cpp/a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+using ui = unsigned int;
+
+int main()
+{
+  ui a, b, h;
+  cin >> a >> b >> h;
+
+  cout << ((a + b) * h) / 2 << endl;
+  return 0;
+}
