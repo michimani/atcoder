fd2fa5e3192ad3231fc8e70b94d64f22098ba4fb
michimani <michimani210@gmail.com>
2024-05-18 04:18:51 +0900

    abc: 030 a

---
+++ abc/001-100/030/README.md
@@ -0,0 +1,4 @@
+AtCoder Beginner Contest 030
+===
+
+[問題 - AtCoder Beginner Contest 030](https://atcoder.jp/contests/abc030/tasks)
---
+++ abc/001-100/030/cpp/a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  double a, b, c, d;
+  cin >> a >> b >> c >> d;
+
+  if (b / a > d / c)
+    cout << "TAKAHASHI" << endl;
+  else if (b / a < d / c)
+    cout << "AOKI" << endl;
+  else
+    cout << "DRAW" << endl;
+
+  return 0;
+}
