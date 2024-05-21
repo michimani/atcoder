70cc483a805cf2c6fb9772d6dbc3b4286167c221
michimani <michimani210@gmail.com>
2024-05-22 00:09:52 +0900

    abc: 082 a

---
+++ abc/001-100/082/README.md
@@ -0,0 +1,4 @@
+AtCoder Beginner Contest 082
+===
+
+[問題 - AtCoder Beginner Contest 082](https://atcoder.jp/contests/abc082/tasks)
---
+++ abc/001-100/082/cpp/a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+using ui = unsigned int;
+
+int main()
+{
+  ui a, b;
+  cin >> a >> b;
+
+  cout << (a + b) / 2 + ui((a + b) % 2 != 0) << endl;
+
+  return 0;
+}
