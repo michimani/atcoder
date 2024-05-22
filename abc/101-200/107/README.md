bcea0058ac7aedf162a833d5b2c3436f8b0ab0ea
michimani <michimani210@gmail.com>
2024-05-23 03:56:50 +0900

    abc: 107 a

---
+++ abc/101-200/107/README.md
@@ -0,0 +1,4 @@
+AtCoder Beginner Contest 107
+===
+
+[問題 - AtCoder Beginner Contest 107](https://atcoder.jp/contests/abc107/tasks)
---
+++ abc/101-200/107/cpp/a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+using ui = unsigned int;
+
+int main()
+{
+  ui n, i;
+  cin >> n >> i;
+
+  cout << n - i + 1 << endl;
+
+  return 0;
+}
