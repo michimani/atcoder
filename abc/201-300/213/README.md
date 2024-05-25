8c760b72af88d574fa1c2bf84d99cd55428baf13
michimani <michimani210@gmail.com>
2024-05-25 23:22:21 +0900

    abc: 213 a,b

---
+++ abc/201-300/213/README.md
@@ -0,0 +1,4 @@
+AtCoder Beginner Contest 213
+===
+
+[問題 - AtCoder Beginner Contest 213](https://atcoder.jp/contests/abc213/tasks)
---
+++ abc/201-300/213/cpp/a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+using ui = unsigned int;
+
+int main()
+{
+  ui a, b;
+  cin >> a >> b;
+
+  cout << (a ^ b) << endl;
+
+  return 0;
+}
