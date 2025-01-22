a5edbcf36b368e4a54620f8735fd3ffb5b117199
michimani <michimani210@gmail.com>
2025-01-22 23:22:08 +0900

    abc: 388 a

---
+++ abc/301-400/388/README.md
@@ -0,0 +1,4 @@
+AtCoder Beginner Contest 388
+===
+
+[問題 - AtCoder Beginner Contest 388](https://atcoder.jp/contests/abc388/tasks)
---
+++ abc/301-400/388/cpp/a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  string s;
+  cin >> s;
+
+  cout << s[0];
+  cout << "UPC" << endl;
+
+  return 0;
+}
