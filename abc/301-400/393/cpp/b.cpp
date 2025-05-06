64deb32b1149cb4598f66e0f2b789e4de2cbfa6b
michimani <michimani210@gmail.com>
2025-05-07 01:43:09 +0900

    abc: 393 b

--- abc/301-400/393/cpp/b.cpp
+++ abc/301-400/393/cpp/b.cpp
@@ -8,17 +8,10 @@ int main()
   string s;
   cin >> s;
 
-  ui i;
   ui ans = 0;
-  ui d = 0;
-
-  while (d * 2 + 3 <= s.length())
-  {
-    for (i = 0; i < s.length() - 2 - d * 2; i++)
+  for (ui d = 0; d * 2 + 3 <= s.length(); d++)
+    for (ui i = 0; i < s.length() - 2 - d * 2; i++)
       ans += ui(s[i] == 'A' && s[i + d + 1] == 'B' && s[i + d * 2 + 2] == 'C');
-
-    d++;
-  }
 
   cout << ans << endl;
 
